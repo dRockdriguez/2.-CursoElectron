491365e7ea157df869ef8b3cb635df63e0a9b753
drockdriguez <david.rodriguez@grupoonetec.com>
2019-04-10 11:40:34 +0200

    .

---
+++ renderer.js
@@ -0,0 +1,16 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+const { ipcRenderer } = require('electron')
+
+
+// envia el mensaje al processo channel1
+ipcRenderer.send('channel1', 'Hello from renderer.js')
+
+ipcRenderer.on('channel1', (e, args) => {
+    console.log('Receiver ::: ' + args)
+})
+
+ipcRenderer.on('private', (e, args) => {
+    console.log('Receiver private ::: ' + args)
+})
