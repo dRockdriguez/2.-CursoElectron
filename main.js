2a04dde97a35c2ae31a2d7418b342c85a9189d20
drockdriguez <david.rodriguez@grupoonetec.com>
2019-04-11 09:29:04 +0200

    webFrame

--- main.js
+++ main.js
@@ -29,14 +29,14 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+   mainWindow.loadFile('index.html')
 
   mainWindow.webContents.on('did-finish-load', () => {
     mainWindow.webContents.send('private', 'Message from MainProcess to mainWindow')
   })
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+ //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
